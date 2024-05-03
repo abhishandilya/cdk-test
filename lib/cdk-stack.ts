32a629a97c410abba3effdc1b85a6c1305f47cc9
Abhishek Shandilya <97976470+abhishandilya@users.noreply.github.com>
2024-05-02 22:38:08 -0400

    remove imports

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,7 +1,4 @@
 import * as cdk from "aws-cdk-lib";
-import { HttpApi } from "aws-cdk-lib/aws-apigatewayv2";
-import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 
 export class CdkStack extends cdk.Stack {
