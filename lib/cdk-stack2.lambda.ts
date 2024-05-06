00d9dbd458114e4ed0cb29d6143593f2d319006f
Abhishek Shandilya <97976470+abhishandilya@users.noreply.github.com>
2024-05-06 14:24:19 -0400

    update lambda to check cache usage

--- lib/cdk-stack2.lambda.ts
+++ lib/cdk-stack2.lambda.ts
@@ -2,7 +2,7 @@ export const handler = async (event: any = {}): Promise<any> => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello from Lambda!",
+      message: "Hello from Lambda2!",
       input: event,
     }),
   };
