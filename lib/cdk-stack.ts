fd19655328d6da371be606d3aba315cd6795cabf
Abhishek Shandilya <97976470+abhishandilya@users.noreply.github.com>
2024-05-06 15:50:47 -0400

    use node 20 runtime lambda

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from "aws-cdk-lib";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 
@@ -6,6 +7,8 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new NodejsFunction(this, "lambda", {});
+    new NodejsFunction(this, "lambda", {
+      runtime: Runtime.NODEJS_20_X,
+    });
   }
 }
