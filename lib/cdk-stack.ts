59d27196bd92ccbe44b7395243a1d72956a2f8a0
Abhishek Shandilya <97976470+abhishandilya@users.noreply.github.com>
2024-05-06 14:09:11 -0400

    add a lambda

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,8 +1,11 @@
 import * as cdk from "aws-cdk-lib";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new NodejsFunction(this, "lambda", {});
   }
 }
